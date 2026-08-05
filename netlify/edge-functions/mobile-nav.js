524c55969250bca28f910b6406d409af882d8dfe
mikerjens <mk@fixer.fo>
2026-08-05 18:32:31 +0100

    Load exact scene matching fix for scene 11A

--- netlify/edge-functions/mobile-nav.js
+++ netlify/edge-functions/mobile-nav.js
@@ -64,7 +64,7 @@ export default async (request, context) => {
 
   const portalScripts = [
     '<script src="/sev-portal-stable-v1.js?v=cc45112b14ae1aa825e27e0f191fc0500608bf94" defer></script>',
-    '<script src="/scene-4a-elduvik-v1.js?v=07d434cfdd0e8e61d0fe3781c41e879c9c120bc8" defer></script>'
+    '<script src="/scene-4a-elduvik-v1.js?v=03dd6468a630ca2c27ddcf2fb6d5a7b8ac69206c" defer></script>'
   ].join('');
 
   html = html
