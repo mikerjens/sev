ca2c2be8d1222e247d1b634a74785244b6c5a935
mikerjens <mk@fixer.fo>
2026-08-19 09:26:55 +0100

    Load updated Aug 19 production status banner

--- netlify/edge-functions/mobile-nav.js
+++ netlify/edge-functions/mobile-nav.js
@@ -134,7 +134,8 @@ export default async (request, context) => {
     '<script src="/location-skalabudin-link-v1.js?v=cab50fb49dfd9247b274725fd5d44adfe3edd1ed" defer></script>',
     '<script src="/hide-filmed-from-schedule-v1.js?v=25f5356fbe2bdb7ca9a339dd28c14f5895861193" defer></script>',
     '<script src="/production-aug22-authoritative-v1.js?v=1492e13f09f5019f6873ee25e5fe6a9dd8f11017" defer></script>',
-    '<script src="/filmed-aug17-cleanup-v1.js?v=ea194bf0ea25796003203c82c8ab36b6c8f659a7" defer></script>'
+    '<script src="/filmed-aug17-cleanup-v1.js?v=ea194bf0ea25796003203c82c8ab36b6c8f659a7" defer></script>',
+    '<script src="/status-banner-aug19-v1.js?v=88eee30dde2381462769abe7729ff2b9784ed8cb" defer></script>'
   ].join('');
 
   html = html
